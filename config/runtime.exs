a7cde95ea7a3d41dfdd4ce33c51902f781293384
Niklas L <niklas@demtrees.com>
2023-10-10 21:20:32 +0300

    Adjust settings to avoid app from crashing constantly in prod

--- config/runtime.exs
+++ config/runtime.exs
@@ -38,7 +38,7 @@ if config_env() == :prod do
     url: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
     socket_options: []
-
+    prepare: :unnamed
   # The secret key base is used to sign/encrypt cookies and other secrets.
   # A default value is used in config/dev.exs and config/test.exs but you
   # want to use a different value for prod and you most likely don't want
